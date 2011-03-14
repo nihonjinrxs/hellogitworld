b0118f991d053be4d34263affdd43b2a2c4af7bd
Matthew McCullough <matthewm@ambientideas.com>
2011-03-14 13:12:41 -0600

    Mainline bugfix

--- cool/hello.groovy
+++ cool/hello.groovy
@@ -1,13 +1,6 @@
 println "hello world!"
 //CHANGEAGAINValeri
-<<<<<<< HEAD
-=======
 //CHANGEAGAIN
->>>>>>> 1bfb8699c71022c913cd4098b48be7d85a1b77b8
-<<<<<<< .merge_file_a04820
 //MasterChangeValeri
-=======
->>>>>>> 43d47201d42c264dcd231eec9836f85a95a7400a
-=======
 //BRANCHCHANGEValeri
->>>>>>> .merge_file_a02268
+//MasterChange
